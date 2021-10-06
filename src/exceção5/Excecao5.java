a70019a9ede524fe97209ed8594b5bf4a1058b54
Rodrigo Wanzeler <wanzervai@gmail.com>
2021-10-06 17:27:39 -0300

    Exception Solution

---
+++ src/exceção5/Excecao5.java
@@ -0,0 +1,45 @@
+package exceção5;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities5.Account;
+import model.exceptions2.BusinessException;
+
+public class Excecao5 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+				
+		System.out.println("Enter account data ");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+				
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+	
+		try {
+			acc.withdraw(amount);
+			System.out.printf("New balance: %.2fn", acc.getBalance());
+		}
+		catch(BusinessException e) {
+			System.out.println(e.getMessage());
+		}
+				
+		sc.close();
+
+	}
+
+}
